337e4ec746fea638e499ce54b3d966bb4a0d9266
sososoosoo <8216kimsc@gmail.com>
2025-09-14 23:09:02 +0900

    초기 저장

---
+++ src/main/java/com/whereclass/WhereclassApplication.java
@@ -0,0 +1,13 @@
+package com.whereclass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhereclassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhereclassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/whereclass/WhereclassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whereclass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhereclassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
